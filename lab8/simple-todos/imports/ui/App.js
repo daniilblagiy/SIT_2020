adca121e6716748c9be3a39a24db2d21c89f070e
Daniil Blagiy <daniilblagiy.work@gmail.com>
2020-04-17 10:02:29 +0300

    lab8: added incomplete tasks count feature

--- lab8/simple-todos/imports/ui/App.js
+++ lab8/simple-todos/imports/ui/App.js
@@ -51,7 +51,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+		  <h1>Todo List ({this.props.incompleteCount})</h1>
 
 		  <label className="hide-completed">
             <input
@@ -83,5 +83,6 @@ class App extends Component {
 export default withTracker(() => {
 	return {
 	  tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+	  incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
 	};
   })(App);
