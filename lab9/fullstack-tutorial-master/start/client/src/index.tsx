336d2fdd59de359614b435087fd1454cbb177b36
Daniil Blagiy <daniilblagiy.work@gmail.com>
2020-05-05 15:34:24 +0300

    lab 10: fixed issues in pages/launches

--- lab9/fullstack-tutorial-master/start/client/src/index.tsx
+++ lab9/fullstack-tutorial-master/start/client/src/index.tsx
@@ -3,6 +3,11 @@ import gql from "graphql-tag";
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import Pages from './pages';
+import injectStyles from './styles';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -29,3 +34,11 @@ client
     `
   })
   .then(result => console.log(result));
+
+  injectStyles();
+  ReactDOM.render(
+	<ApolloProvider client={client}>
+	  <Pages />
+	</ApolloProvider>, 
+	document.getElementById('root')
+  );
